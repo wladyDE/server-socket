6c0c4fd9e7e5b2035232889486754ed156e2a59f
Wlady <Volodymyr.Havryliuk@quinscape.de>
2023-03-22 15:02:23 +0100

    add body&headers&parameters logging

--- src/main/java/org/example/MainServlet.java
+++ src/main/java/org/example/MainServlet.java
@@ -7,8 +7,11 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.Map;
 
 
 @WebServlet(urlPatterns = "/main")
@@ -49,6 +52,11 @@ public class MainServlet extends HttpServlet {
             resp.setContentLength(MESSAGE.length());
 
             PrintWriter writer = resp.getWriter();
+
+            logParameters(req);
+            logBody(req);
+            logHeaders(req);
+
             writer.print(MESSAGE);
             writer.flush();
             writer.close();
@@ -58,5 +66,35 @@ public class MainServlet extends HttpServlet {
             LOGGER.error(String.format("Error processing request: %s", e.getMessage()));
             resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Error processing request");
         }
+    }
+
+    private void logHeaders(HttpServletRequest req) throws IOException {
+        Enumeration<String> headerNames = req.getHeaderNames();
+        LOGGER.info("Requests headers:");
+        while (headerNames.hasMoreElements()) {
+            String headerName = headerNames.nextElement();
+            String headerValue = req.getHeader(headerName);
+            LOGGER.info(String.format("%s : %s", headerName, headerValue));
+        }
+    }
+
+    private void logParameters(HttpServletRequest req) throws IOException {
+        Map<String, String[]> params = req.getParameterMap();
+        for (String key : params.keySet()) {
+            String value = params.get(key)[0];
+            LOGGER.info(String.format("Request parameter: %s = %s", key, value));
+        }
+    }
+
+    private void logBody(HttpServletRequest req) throws IOException {
+        BufferedReader reader = req.getReader();
+        StringBuilder body = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            body.append(line);
+        }
+        reader.close();
+        String requestBody = body.toString();
+        LOGGER.info(String.format("Request body: %s", requestBody));
     }
 }
